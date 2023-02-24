5f5c510912751b519c99a7fe664a47df81f7751d
sydneydoyle <106398877+sydneydoyle@users.noreply.github.com>
2023-02-24 00:52:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+import $ from "https://cdn.skypack.dev/jquery@3.6.0";
+import domJs from "https://cdn.skypack.dev/dom.js@0.2.0";
+import * as bootstrap from "https://cdn.skypack.dev/bootstrap@5.1.3";
+import fontAwesome from "https://cdn.skypack.dev/font-awesome@4.7.0";
+
 /*! Fades out the whole page when clicking links */
 $('a').click(function(e) {
 e.preventDefault();
