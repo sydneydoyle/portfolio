953cc8442e8b027184c67d1552082333c79a4cc5
sydneydoyle <106398877+sydneydoyle@users.noreply.github.com>
2023-02-24 00:56:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-import $ from "https://cdn.skypack.dev/jquery@3.6.0";
-import domJs from "https://cdn.skypack.dev/dom.js@0.2.0";
-import * as bootstrap from "https://cdn.skypack.dev/bootstrap@5.1.3";
-import fontAwesome from "https://cdn.skypack.dev/font-awesome@4.7.0";
-
 /*! Fades out the whole page when clicking links */
 $('a').click(function(e) {
 e.preventDefault();
@@ -19,4 +14,4 @@ $(document).ready(function(){
 $('body').css('display', 'none');
 $('body').fadeIn(500);
 
-}); 
+});
